3a7a44a4a55091666dc660e066a5501bb186aa20
FrancoStoll <francostoll@gmail.com>
2023-09-25 13:05:34 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import ReactModal from "react-modal";
 import Boleta from "./components/Boleta";
 import Formulario from "./components/Formulario";
-import Header from "./components/header";
+
 import { BoletaProvider } from "./context/BoletaProvider";
-import useBoleta from "./hooks/useBoleta";
-import Modal from "react-modal";
 import ModalEditar from "./components/ModalEditar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import React from "react";
+import Header from "./components/header";
 
 function App() {
   return (
     <BoletaProvider>
-      <Header />
+      {/* <Header /> */}
 
       <div className="text-3xl text-center p-2.5 mb-10">
         <p className="font-bold py-4 ">Crear boleta de compra</p>
       </div>
-
-      
 
       <main className="md:flex">
         <Formulario />
